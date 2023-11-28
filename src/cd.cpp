726b386f191ab8eb4af67b77e4e113e0b5624b25
MT23046 <143582579+MT23046@users.noreply.github.com>
2023-11-28 19:19:05 +0530

    cd .. & cd ~

--- src/cd.cpp
+++ src/cd.cpp
@@ -6,6 +6,12 @@ using namespace std;
 void CD::execute(istream &is, filesystem::path &path) {
   string target;
   is >> target;
+  if (target == ".." or target == "~") {
+    path = (target == ".." ? path.parent_path() : filesystem::path(getenv("HOME")));
+    cout << "Switched to\n" << path << '\n';
+    return;
+  }
+  
   filesystem::path newPath = path / target;
   if (filesystem::exists(newPath) && filesystem::is_directory(newPath)) {
     cout << "Switched from\n" << path << "\nto\n" << newPath << '\n';
